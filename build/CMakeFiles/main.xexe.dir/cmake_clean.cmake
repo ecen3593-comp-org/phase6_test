9e423ef45d23e7a02db00f4723159c37d0b71002
PSCurlin <phaedra.curlin@outlook.fr>
2023-03-29 13:26:54 -0600

    added test files

---
+++ build/CMakeFiles/main.xexe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.xexe.dir/src/phase6_test.s.o"
+  "main.xexe.pdb"
+  "main.xexe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM)
+  include(CMakeFiles/main.xexe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
